d0f95ef57f66265e2188bf3b5f02868809c36689
Sara Hackle <Shackle@ivytech.edu>
2022-05-05 20:42:47 -0500

    Tags from Application can now be deleted

--- AppTrackerMVC/Repositories/TagRepository.cs
+++ AppTrackerMVC/Repositories/TagRepository.cs
@@ -63,7 +63,7 @@ namespace AppTrackerMVC.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"SELECT t.Id, t.TagName, at.Id AS AppTagId, at.TagId, at.ApplicationId 
-                        FROM Tag t 
+                        FROM Tag t
                         Join ApplicationTag at ON t.Id = at.TagId
                         Join Application a ON at.ApplicationId = a.Id
                         Where a.Id = @id";
@@ -73,7 +73,8 @@ namespace AppTrackerMVC.Repositories
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
 
-                        List<Tag> appTags = new List<Tag>();
+                        List<Tag> tags = new List<Tag>();
+
 
                         while (reader.Read())
                         {
@@ -83,7 +84,57 @@ namespace AppTrackerMVC.Repositories
                                 TagName = DbUtils.GetString(reader, "TagName")
                             };
 
-                            appTags.Add(tag);
+                            tags.Add(tag);
+
+                        }
+
+                        return tags;
+
+                    }
+                }
+            }
+
+
+        }
+
+        public List<ApplicationTag> GetApplicationTagsByApplicationId(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT t.Id, t.TagName, at.Id AS AppTagId, at.TagId, at.ApplicationId 
+                        FROM ApplicationTag at
+                        Join Tag t ON t.Id = at.TagId
+                        Join Application a ON at.ApplicationId = a.Id
+                        Where a.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+
+                        List<ApplicationTag> appTags = new List<ApplicationTag>();
+
+                        while (reader.Read())
+                        {
+
+
+                            ApplicationTag appTag = new ApplicationTag()
+                            {
+                                Id = DbUtils.GetInt(reader, "AppTagid"),
+                                TagId = DbUtils.GetInt(reader, "TagId"),
+                                ApplicationId = DbUtils.GetInt(reader, "ApplicationId"),
+
+                                Tag = new Tag()
+                                {
+                                    Id = DbUtils.GetInt(reader, "Id"),
+                                    TagName = DbUtils.GetString(reader, "TagName")
+                                }
+                            };
+                            appTags.Add(appTag);
                         }
 
                         return appTags;
@@ -95,6 +146,43 @@ namespace AppTrackerMVC.Repositories
 
         }
 
+
+        public ApplicationTag GetApplicationTagById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, TagId, ApplicationId 
+                        FROM ApplicationTag  
+                        
+                        Where Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        ApplicationTag appTags = null;
+
+                        if (reader.Read())
+                        {
+                            ApplicationTag appTag = new ApplicationTag()
+                            {
+                                TagId = DbUtils.GetInt(reader, "TagId"),
+                                ApplicationId = DbUtils.GetInt(reader, "ApplicationId")
+                            };
+
+
+                        }
+
+
+                        return appTags;
+                    }
+                }
+            }
+        }
         public void AddApplicationTag(ApplicationTag appTag)
         {
             using (SqlConnection conn = Connection)
@@ -110,10 +198,28 @@ namespace AppTrackerMVC.Repositories
 
                     cmd.Parameters.AddWithValue("@tagId", appTag.TagId);
                     cmd.Parameters.AddWithValue("@applicationId", appTag.ApplicationId);
-                  
+
 
                     appTag.Id = (int)cmd.ExecuteScalar();
                 }
+            }
+        }
+
+        public void DeleteApplicationTag(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                     DELETE from ApplicationTag
+                     WHERE Id = @id ";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+
             }
         }
     }
